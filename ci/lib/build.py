592314d8acaa0b5a3c1d8fc30d54b6c85b011276
Daniel Cahn <daniel.cahn@instabase.com>
2021-09-27 13:55:19 -0400

    skip dirs

--- ci/lib/build.py
+++ ci/lib/build.py
@@ -5,6 +5,7 @@ import os
 import zipfile
 from io import BytesIO
 
+TO_SKIP = ['__pycache__', '.DS_Store', 'example']
 
 def zip_project(root_location: str) -> bytes:
     """
@@ -13,9 +14,15 @@ def zip_project(root_location: str) -> bytes:
     logging.info(f"Zipping project at {root_location}")
     bytesio: BytesIO = BytesIO()
     with zipfile.ZipFile(bytesio, "w") as zip_file:
-        for root, dirs, files in os.walk(root_location):
+        for root, dirs, files in os.walk(root_location, ):
+            if any(f"/{i}/" in root or root.endswith(f"/{i}") for i in TO_SKIP): # TODO Def a better way to do this
+                logging.debug(f"Skipping dir {root}")
+                continue
             for file in files:
-                if file in [".DS_Store", "__pycache__", "example"]: # TODO hardcoding example here is bad
+                if file.startswith("."):
+                    logging.debug(f"Skipping file {file}")
+                    continue
+                if file in [".DS_Store", "__pycache__"]:
                     continue
                 logging.debug(f"Adding file {file} to zip")
                 file_path = os.path.join(root, file)
