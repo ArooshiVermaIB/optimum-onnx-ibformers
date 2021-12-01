1c405d1b53bb665325afcb77d5ab78a24246dda3
Rafal Powalski <rafal.powalski@instabase.com>
2021-12-01 14:18:09 +0100

    refactor config kwargs

--- ibformers/trainer/train_utils.py
+++ ibformers/trainer/train_utils.py
@@ -1,4 +1,6 @@
-from datasets import Dataset, DatasetDict
+from typing import Dict
+
+from datasets import Dataset, DatasetDict, ClassLabel
 
 from ibformers.utils import exceptions
 
@@ -28,3 +30,20 @@ def split_train_with_column(dataset: Dataset):
         splitted_dataset[k] = train_ds.select(indices=indices[k])
 
     return splitted_dataset
+
+
+def prepare_config_kwargs(ds: Dataset) -> Dict:
+    """
+    Function prepares a dictionary of parameters suited to given dataset.
+    Additonal kwargs will be passed to the model config for extraction or classification task
+    """
+    features = ds.features
+    if isinstance(features["labels"].feature, ClassLabel):
+        label_list = features["labels"].feature.names
+    else:
+        raise ValueError(f"Dataset labels column is not of required type - ClassLabel")
+    label_to_id = {l: i for i, l in enumerate(label_list)}
+    kwargs = dict(
+        num_labels=len(label_list), label_to_id=label_to_id, id2label={i: lab for lab, i in label_to_id.items()}
+    )
+    return kwargs
