0424043716d6b5cc662e442e80012840bed3d9da
Bartosz Topolski <bartosz.topolski@instabase.com>
2022-01-24 15:03:25 +0100

    Add whole word masking to bbox augmenter. (#108)

--- ibformers/data/collators/augmenters/__init__.py
+++ ibformers/data/collators/augmenters/__init__.py
@@ -1,2 +1,3 @@
 from .bbox import BboxAugmenter
+from .bbox_masking import BboxMaskingAugmenter
 from .mlm import MLMAugmenter
--- ibformers/data/collators/augmenters/bbox_masking.py
+++ ibformers/data/collators/augmenters/bbox_masking.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from typing import Dict
 
 import torch
+from transformers import DataCollatorForWholeWordMask
 
 from ibformers.data.collators.augmenters.base import BaseAugmenter
 
@@ -10,31 +11,63 @@ from ibformers.data.collators.augmenters.base import BaseAugmenter
 class BboxMaskingAugmenter(BaseAugmenter):
     """
     Augmenter for masked bbox prediction training task.
+
+    Attributes:
+        bbox_masking_probability: the probability for the token to be masked
+        bbox_masking_whole_word_mask: If True, whole un-tokenized tokens are masked.
+        bbox_masking_value: Custom value for masked bbox coordinate
+        bbox_masking_replace_percentage: The probability that masked token will be replaced by masking value.
+         Allows similar flow to MLM, where part of the tokens selected for masking will be left unmasked.
     """
 
     bbox_masking_probability: float = 0.15
-    bbox_masking_whole_word_mask: float = False
+    bbox_masking_whole_word_mask: bool = False
     bbox_masking_value: int = None
     bbox_masking_replace_percentage: float = 1.0
 
     def __post_init__(self):
         if self.bbox_masking_value is None:
             self.bbox_masking_value = getattr(self.model.config, "max_2d_position_embeddings", 1024) - 1
+        self.wwm_collator = DataCollatorForWholeWordMask(
+            tokenizer=self.tokenizer, mlm=True, mlm_probability=self.bbox_masking_probability
+        )
 
     def augment(self, batch: Dict[str, torch.Tensor]):
-        bbox = batch["bbox"]
-        bbox_labels = batch["bbox"].clone()
+        if self.bbox_masking_whole_word_mask:
+            masked_indices = self._get_mask_wwm(batch)
+        else:
+            masked_indices = self._get_mask(batch)
 
+        return self._mask_tokens(batch, masked_indices)
+
+    def _get_mask(self, batch: Dict[str, torch.Tensor]):
+        bbox_labels = batch["bbox"]
         special_tokens_mask = [
             self.tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True)
             for val in batch["input_ids"].tolist()
         ]
         special_tokens_mask = torch.tensor(special_tokens_mask, dtype=torch.bool)
-        base_shape = bbox_labels.shape[:-1]
-        probability_matrix = torch.full(base_shape, self.bbox_masking_probability)
-
+        probability_matrix = torch.full(bbox_labels.shape[:-1], self.bbox_masking_probability)
         probability_matrix.masked_fill_(special_tokens_mask, value=0.0)
         masked_indices = torch.bernoulli(probability_matrix).bool()
+        return masked_indices
+
+    def _get_mask_wwm(self, batch: Dict[str, torch.Tensor]):
+        # workaround: let the MLM collator prepare masks, and then propagate them onto the bboxes
+
+        # depending whether the MLM augmentation is before or after
+        if "labels" in batch:
+            input_col = torch.where(batch["labels"] == -100, batch["input_ids"], batch["labels"])
+        else:
+            input_col = batch["input_ids"]
+        out_dict = self.wwm_collator.torch_call(input_col.tolist())
+
+        return out_dict["input_ids"] == self.tokenizer.mask_token_id
+
+    def _mask_tokens(self, batch: Dict[str, torch.Tensor], masked_indices: torch.Tensor):
+        bbox = batch["bbox"]
+        bbox_labels = batch["bbox"].clone()
+        base_shape = bbox_labels.shape[:-1]
         bbox_labels[~masked_indices] = -100  # We only compute loss on masked tokens
 
         # most of the time, we replace masked bbox with mask value
