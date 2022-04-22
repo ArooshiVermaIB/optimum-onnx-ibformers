b1c679a2a4f89a14564abb02f4bd9700a5b71e4e
Josh Xie <90649925+qxie3@users.noreply.github.com>
2022-04-22 09:54:46 -0700

    [INSIGHTS-3158] support table in refiner (#189)
    * support table in refiner
    
    * fix
    
    * address comments
    
    * fix

--- ibformers/trainer/refiner_module_generator.py
+++ ibformers/trainer/refiner_module_generator.py
@@ -10,19 +10,20 @@ _MIN_CONFIDENCE_DEFAULT_VALUE = "0.90"
 
 _PUBLISHED_MODEL_COL_NAME = "__PUBLISHED_MODEL_VERSION"
 _PUBLISHED_MODEL_VALUE = "'{{published_model_version}}'"
+_PUBLISHED_MODEL_NAME = "__PUBLISHED_MODEL_NAME"
 
 
-def _make_refiner_field(label: str, function: str) -> ibrefiner_prog_pb2.RefinerField:
+def _make_refiner_field(label: str, function: str, output_type: str = "") -> ibrefiner_prog_pb2.RefinerField:
     return ibrefiner_prog_pb2.RefinerField(
         desc=ibrefiner_prog_pb2.RefinerFieldDescription(
-            output_type="",
+            output_type=output_type,
             label=label,
         ),
         req=ibrefiner_prog_pb2.RefinerRequest(expression=function, type=ibrefiner_prog_pb2.REFINER_FUNC),
     )
 
 
-def _make_run_model_script_contents(model_name: str) -> str:
+def _make_run_model_script_contents() -> str:
     return f"""
 from typing import Any, Mapping, Union, List
 from ib.market.ib_intelligence.functions import IntelligencePlatform, kwargs_to_ms_params, resolve_input, log
@@ -32,6 +33,28 @@ from instabase.provenance.tracking import Value
 # we expect this format to come out ner_result_to_values(), and is used as input for get_confidence() and get_field()
 ModelResultType =  Value[Mapping[str, Value[List[Value[List[Union[Value[str], float]]]]]]]
 ModelResultNoProvType = Mapping[str, List[List[Union[str, float]]]]
+
+
+def table_result_to_values(result: Any, INPUT_COL: Value[str]) -> ModelResultType:
+    raw_data: bytes = result.get('raw_data', {{}}).get('data', [])
+    json_result = json.loads(raw_data.decode("utf-8"))
+    fields = json_result['fields']
+    results = {{}}
+
+    for field in fields:
+        table_annotations = field['table_annotations']
+        results[field['field_name']] = []
+        for table_annotation in table_annotations:
+            row_len, col_len = len(table_annotation['rows']), len(table_annotation['cols'])
+            cells = [['' for _ in range(col_len)] for i in range(row_len)]
+            for cell in table_annotation['cells']:
+                # TODO(qxie3): handle merge cell
+                # we currently assume start_index == end_index before supporting merge cell
+                cells[cell['row_start_index']][cell['col_start_index']] = INPUT_COL[cell['start_index']:cell['end_index']]
+            results[field['field_name']].append(cells)
+
+    return Value(results)
+
 
 def ner_result_to_values(result: Any, INPUT_COL: Value[str]) -> ModelResultType:
   entities = result.get('ner_result', {{}}).get('entities', [])
@@ -52,6 +75,7 @@ def ner_result_to_values(result: Any, INPUT_COL: Value[str]) -> ModelResultType:
     )
 
   return Value(fields)
+
 
 def average(values: List[float], **kwargs) -> Union[float, str]:
   if len(values) == 0:
@@ -81,9 +105,13 @@ def get_field(MODEL_RESULT_COL: ModelResultType, field_name: Value[str], **kwarg
     Returns the extracted values, joined together by whitespace.
 
   \"\"\"
-  field_values =  MODEL_RESULT_COL.value().get(field_name.value(), Value([]))
-  vals = [x.value()[0] for x in field_values.value()]  
-  return Value.join(' ', vals)
+  if 'result_type' in MODEL_RESULT_COL.value() and MODEL_RESULT_COL.value().get('result_type') == 'Table':
+    field_values =  MODEL_RESULT_COL.value().get(field_name.value(), Value([])) 
+    return Value(field_values.value()[0])
+  else:
+    field_values =  MODEL_RESULT_COL.value().get(field_name.value(), Value([]))
+    vals = [x.value()[0] for x in field_values.value()]  
+    return Value.join(' ', vals)
 
 # type for error messages
 ErrorStr = str
@@ -150,20 +178,25 @@ def run_model_no_provenance(INPUT_COL: str, model_version: str, **kwargs) -> Any
   \"\"\"
   raise NotImplementedError("Provenance Tracking must be on to run a model. Please turn it on in File > Settings.")
 
-def run_model(INPUT_COL: Value[str], model_version: Value[str], **kwargs) -> ModelResultType:
+def run_model(INPUT_COL: Value[str], model_name: Value[str], model_version: Value[str], **kwargs) -> ModelResultType:
   ip_sdk = IntelligencePlatform(kwargs)
   ibocr, err = kwargs['_FN_CONTEXT_KEY'].get_by_col_name('INPUT_IBOCR_RECORD')
   if err:
     raise KeyError(err)
   record = ParsedIBOCRBuilder(use_ibdoc=True)
   record.add_ibocr_records([ibocr])
-  results = ip_sdk.run_model('{model_name}', 
+  results = ip_sdk.run_model(model_name.value(),
                           input_record=record, 
                           force_reload=False,
                           refresh_registry=False,
                           model_version=model_version.value(),
                           **kwargs)
-  return ner_result_to_values(results, INPUT_COL)
+  if 'ner_result' in results:
+      return ner_result_to_values(results, INPUT_COL)
+  elif 'raw_data' in results and results['raw_data'].get('type') == 'Table':
+      return table_result_to_values(results, INPUT_COL)
+  else:
+      raise TypeError('run_model returns unexpected result type.')
 
 def register(name_to_fn):
   name_to_fn.update({{
@@ -199,6 +232,7 @@ def write_refiner_program(
     labels: List[str],
     model_name: str,
     dev_input_path: str,
+    is_table: bool = False,
 ) -> str:
     """Writes a refiner module to the training job output, which can be imported
     into a Flow. Returns the local path to the Refiner program.
@@ -215,7 +249,7 @@ def write_refiner_program(
 
     # write script that can load model
     script_path = os.path.join(refiner_scripts_path, "run_model.py")
-    script_content = _make_run_model_script_contents(model_name)
+    script_content = _make_run_model_script_contents()
     with open(script_path, "w+") as f:
         f.write(script_content)
 
@@ -243,14 +277,27 @@ def write_refiner_program(
     model_version_field.desc.description = "Set the model version published in Marketplace to use in this Refiner."
     ibrefiner.fields.append(model_version_field)
 
+    model_name_field = _make_refiner_field(_PUBLISHED_MODEL_NAME, f"echo('{model_name}')")
+    model_name_field.desc.description = "Set the model name published in Marketplace to use in this Refiner."
+    ibrefiner.fields.append(model_name_field)
+
     # create hidden field for model result
-    ibrefiner.fields.append(_make_refiner_field("__model_result", f"run_model(INPUT_COL, {_PUBLISHED_MODEL_COL_NAME})"))
+    ibrefiner.fields.append(
+        _make_refiner_field(
+            "__model_result", f"run_model(INPUT_COL, {_PUBLISHED_MODEL_NAME}, {_PUBLISHED_MODEL_COL_NAME})"
+        )
+    )
 
     # create fields for each of extracted_fields
     for label in labels:
         label_no_spaces = label.replace(" ", "_")
         # create main field
-        ibrefiner.fields.append(_make_refiner_field(label_no_spaces, f"get_field(__model_result, '{label}')"))
+        if is_table:
+            ibrefiner.fields.append(
+                _make_refiner_field(label_no_spaces, f"get_field(__model_result, '{label}')", "Table")
+            )
+        else:
+            ibrefiner.fields.append(_make_refiner_field(label_no_spaces, f"get_field(__model_result, '{label}')"))
 
         # create confidence field
         ibrefiner.fields.append(
